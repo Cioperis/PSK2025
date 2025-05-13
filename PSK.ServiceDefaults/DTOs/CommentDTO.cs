eb49569d41ad30cf8381bceff3f0f61d2e477993
Big dude <144787771+RedasLaukzemis@users.noreply.github.com>
2025-05-13 21:56:28 +0300

    merge main into f/SCRUM-47

--- PSK.ServiceDefaults/DTOs/CommentDTO.cs
+++ PSK.ServiceDefaults/DTOs/CommentDTO.cs
@@ -4,6 +4,7 @@ namespace PSK.ServiceDefaults.DTOs;
 
 public class CommentDTO
 {
+    public Guid Id { get; set; }
     [Required(ErrorMessage = "Content is required")]
     public string Content { get; set; }
     [Required(ErrorMessage = "DiscussionId is required")]
