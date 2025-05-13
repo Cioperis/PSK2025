63465500a1575a382be3b243c9d84d4043ba1ab3
Redas Laukžemis <laukzemis.r@gmail.com>
2025-05-13 21:49:07 +0300

    merge main into f/SCRUM-47

--- PSK.ServiceDefaults/DTOs/CommentDTO.cs
+++ PSK.ServiceDefaults/DTOs/CommentDTO.cs
@@ -4,7 +4,6 @@ namespace PSK.ServiceDefaults.DTOs;
 
 public class CommentDTO
 {
-    public Guid Id { get; set; }
     [Required(ErrorMessage = "Content is required")]
     public string Content { get; set; }
     [Required(ErrorMessage = "DiscussionId is required")]
--- PSK.ServiceDefaults/DTOs/DiscussionDTO.cs
+++ PSK.ServiceDefaults/DTOs/DiscussionDTO.cs
@@ -4,8 +4,6 @@ namespace PSK.ServiceDefaults.DTOs;
 
 public class DiscussionDTO
 {
-    [Required(ErrorMessage = "Id is required")]
-    public Guid Id { get; set; }
     [Required(ErrorMessage = "Name is required")]
     public string Name { get; set; }
     public DateTime UpdatedAt { get; set; }
