2b2de4697c86a5df127b9c1c01bc42ca8a27b179
adaspera <jusciokas1@gmail.com>
2025-05-27 01:33:15 +0300

    https

--- PSK.Web/src/api/axiosInstance.ts
+++ PSK.Web/src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 ﻿import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5041',
+    baseURL: 'https://localhost:7262',
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json',
